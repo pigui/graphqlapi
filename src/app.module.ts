e9b23461d46766309c22fa24e7d5a883192c6def
pigui <jgpigui@gmail.com>
2021-06-17 22:46:15 +0200

    install mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: async () => {
+        const mongod = new MongoMemoryServer();
+        const uri = await mongod.getUri();
+        return {
+          uri,
+          useCreateIndex: true,
+          useNewUrlParser: true,
+          useFindAndModify: true,
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
